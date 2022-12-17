6d057a2b43473437c55989edb44f167098e338be
Fabiano <fabiano.pereira@seventh.com.br>
2022-12-17 19:08:09 -0300

    add new screen

--- app/src/main/java/com/fabianospdev/downloadtube/Downloads.kt
+++ app/src/main/java/com/fabianospdev/downloadtube/Downloads.kt
@@ -1,13 +1,11 @@
 package com.fabianospdev.downloadtube
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -18,31 +16,31 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-import java.io.File
 
-class MainActivity : ComponentActivity() {
+class Downloads : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MainContent() }
+        setContent { downloadContent() }
     }
 
     @OptIn(ExperimentalMaterial3Api::class)
-    @Preview
     @Composable
-    private fun MainContent() {
+    @Preview
+    private fun downloadContent() {
         Scaffold(
-            topBar = { MainTopBar() },
-            floatingActionButton = { MainFab() },
-        ) {}
+            topBar = { DownloadTopBar() },
+            content = { Downloadcontent("") },
+        )
     }
 
     @Composable
-    private fun MainTopBar() {
+    private fun DownloadTopBar() {
         SmallTopAppBar(
             title = {
                 Text(
                     text = stringResource(id = R.string.app_name),
-                    color = androidx.compose.ui.graphics.Color.Blue,
+                    color = Color.Blue,
                     fontStyle = FontStyle.Italic,
                     fontWeight = FontWeight.W500,
                     fontFamily = FontFamily.SansSerif,
@@ -57,24 +55,16 @@ class MainActivity : ComponentActivity() {
                 )
             },
             colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                containerColor = androidx.compose.ui.graphics.Color.LightGray,
-                navigationIconContentColor = androidx.compose.ui.graphics.Color.Blue
+                containerColor = Color.LightGray,
+                navigationIconContentColor = Color.Blue
             )
         )
     }
 
     @Composable
-    private fun MainFab() {
-        FloatingActionButton(onClick = { goDownloadActivity() }) {
-            Icon(
-                imageVector = Icons.Filled.Add,
-                contentDescription = "",
-                tint = androidx.compose.ui.graphics.Color.Blue
-            )
-        }
-    }
-
-    private fun goDownloadActivity(){
-        startActivity(Intent(this, Downloads::class.java))
+    private fun Downloadcontent(name: String?) {
+        Text(text = "Conteudo")
     }
 }
+
+
