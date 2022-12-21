3b98cc0a1044a30499ab2be83a6ebeda51f6c23b
Fabiano <fabiano.pereira@seventh.com.br>
2022-12-21 19:00:05 -0300

    Fix error change to second activity

--- app/src/main/java/com/fabianospdev/downloadtube/MainActivity.kt
+++ app/src/main/java/com/fabianospdev/downloadtube/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.fabianospdev.downloadtube
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -18,9 +21,10 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-import java.io.File
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { MainContent() }
@@ -37,44 +41,37 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun MainTopBar() {
-        SmallTopAppBar(
-            title = {
-                Text(
-                    text = stringResource(id = R.string.app_name),
-                    color = androidx.compose.ui.graphics.Color.Blue,
-                    fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.W500,
-                    fontFamily = FontFamily.SansSerif,
-                    letterSpacing = TextUnit.Unspecified,
-                    textDecoration = TextDecoration.Underline,
-                    textAlign = TextAlign.Center,
-                    lineHeight = 35.sp,
-                    overflow = TextOverflow.Clip,
-                    softWrap = false,
-                    maxLines = Int.MAX_VALUE,
-                    style = LocalTextStyle.current,
-                )
-            },
-            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                containerColor = androidx.compose.ui.graphics.Color.LightGray,
-                navigationIconContentColor = androidx.compose.ui.graphics.Color.Blue
+    private fun MainTopBar(){
+        TopAppBar(title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                color = androidx.compose.ui.graphics.Color.Blue,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.W500,
+                fontFamily = FontFamily.SansSerif,
+                letterSpacing = TextUnit.Unspecified,
+                textDecoration = TextDecoration.Underline,
+                textAlign = TextAlign.Center,
+                lineHeight = 35.sp,
+                overflow = TextOverflow.Clip,
+                softWrap = false,
+                maxLines = Int.MAX_VALUE,
+                style = LocalTextStyle.current,
             )
-        )
+        })
     }
 
     @Composable
-    private fun MainFab() {
-        FloatingActionButton(onClick = { goDownloadActivity() }) {
-            Icon(
-                imageVector = Icons.Filled.Add,
-                contentDescription = "",
-                tint = androidx.compose.ui.graphics.Color.Blue
-            )
+    private fun MainFab(){
+        val context = LocalContext.current
+        FloatingActionButton(onClick = { showDownloadActivity(context) }) {
+            Icon(imageVector = Icons.Filled.Add, "add")
         }
     }
 
-    private fun goDownloadActivity(){
-        startActivity(Intent(this, Downloads::class.java))
+    private fun showDownloadActivity(context: Context){
+        context.startActivity(Intent(context, Downloads::class.java))
     }
+
 }
+
